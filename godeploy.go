b2b0ce244fdabee24351b3bbfa124ef22e9b2f7c
Piotr <piotr@positionly.com>
2016-02-19 22:45:51 +0100

    add init action

--- godeploy.go
+++ godeploy.go
@@ -2,42 +2,37 @@ package main
 
 import (
 	"github.com/codegangsta/cli"
+	"io"
 	"os"
 )
 
 func main() {
 	app := cli.NewApp()
 	app.Name = "godeploy"
+	app.EnableBashCompletion = true
 	app.Usage = "Simply deploy binaries to remote server"
 	app.Commands = []cli.Command{
 		{
-			Name:   "init",
-			Usage:  "build config directory and config file",
-			Action: initConfig(),
+			Name:  "init",
+			Usage: "build config directory and config file",
+			Action: func(c *cli.Context) {
+				os.Mkdir("./config", 0777)
+
+				f, err := os.Create("config/config.yml")
+				checkErr(err)
+				defer f.Close()
+
+				io.WriteString(f, "goos: \ngoarch: \n\nenvironment: \nserver: \npath: \n\nslack_webhook: \nstart_msg: \nfinish_msg: \nslack_name: \nslack_emoji: ")
+			},
 		},
 		{
-			Name:   "deploy",
-			Usage:  "build and deploy binary to remote server",
-			Action: deployBinary(),
+			Name:  "deploy",
+			Usage: "build and deploy binary to remote server",
+			// Action: deployBinary,
 		},
 	}
 
 	app.Run(os.Args)
-}
-
-func initConfig() {
-	dir, err := os.Mkdir(config, d)
-	checkErr(err)
-
-	f, err := os.Create("config/config.yml")
-	checkErr(err)
-	defer f.Close()
-
-	w, err := f.Write("goos: \ngoarch: \nenvironment: \nserver: \npath: \nslack_webhook: \nstart_msg: \nfinish_msg: \nslack_name: \nslack_emoji: ")
-	checkErr(err)
-}
-
-func deployBinary() {
 }
 
 func checkErr(e error) {

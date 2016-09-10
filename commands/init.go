37f9917c356f7ec55297f0b8cf90f372ec521323
Jan Makara <jan.makara@hotmail.com>
2016-09-10 20:24:07 +0200

    add example deploy configuration into deploy.yml

--- commands/init.go
+++ commands/init.go
@@ -13,8 +13,33 @@ func Config(c *cli.Context) {
 	checkErr(err)
 	defer f.Close()
 	const (
-		comment = "# see example config file at github.com/piokaczm/godeploy"
-	)
+		comment = `# find more information at github.com/piokaczm/godeploy
+# goos: linux # cross-compilation variables
+# goarch: amd64
+# vendor: true # if you're using vendor experiment and want to filter out vendor dir (damn you go!)
+# test: true # only if you want to run all tests before deploy, defaults to false
+# godep: true # only if you're using godep and want to run godep restore before building a binary, defaults to false
+# strategy: scp # optional
+# binary_name: name # optional - if blank the tool will run plain go build
+#
+# environments:
+#   staging: # it's [environment] for deploy command, name it as you wish
+#     host: example.net # no need for adding a digit when deploying to one server only
+#     user: example_user
+#     path: binaries/
+#     restart_command: etc/init.d/daemon restart
+#   production:
+#     host_1: anotherexample.net # Godzira matches host and user using the provided digit, so make sure to fill it properly
+#     user_1: user_1
+#     host_2: anotherexample2.net
+#     user_2: user_2
+#     path: current/binaries/
+#     restart_command: etc/init.d/daemon restart
+#
+# slack: # optional
+#   webhook: https://hooks.slack.com/services/xxx/xxx # no more custom settings for now, please select emoji, name etc. via Slack
+#   appname: AppName`
+  )
 
 	io.WriteString(f, comment)
 }
